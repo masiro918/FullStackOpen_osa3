e28cfca30850a336d8d2e206cb9c52e1b73ee1fb
masiro918 <matias.siro@outlook.com>
2024-07-18 11:37:18 +0300

    Add excercise 7

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
-const persons = {
+app.use(express.json())
+app.use(morgan('tiny'))
+
+let persons = {
     "persons": [
       {
         "name": "Arto Hellas",
@@ -24,17 +28,17 @@ const persons = {
         "id": "4"
       },
       {
-        "id": 5,
+        "id": "5",
         "name": "Kalle",
         "number": "1234"
       },
       {
-        "id": 6,
+        "id": "6",
         "name": "Mikko",
         "number": "123"
       },
       {
-        "id": 7,
+        "id": "7",
         "name": "Kalvei",
         "number": "123123"
       }
@@ -43,6 +47,51 @@ const persons = {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.persons.find(person => person.id === id)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.persons.filter(person => person.id !== id)
+
+  response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  // todo: add person here
+  const id = Math.floor(Math.random() * 10000)
+  const newPerson = {
+    "id" : id + "",
+    "name": body.name,
+    "number": body.number
+  }
+
+  if (newPerson.name == "" || newPerson.number == "") {
+    return response.status(400).json({
+      error: 'name and number must not be empty'
+    })
+  }
+
+  persons.persons.forEach(person => {
+    if (person.name === newPerson.name) {
+      return response.status(400).json({
+        error: 'name must be unique'
+      })
+    }
+  });
+  persons.persons = persons.persons.concat(newPerson)
+
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send('<p>Phonebook has info for ' + persons.persons.length + ' people<br>' + new Date().toString() + '</p>')
 })
 
 const PORT = 3001
